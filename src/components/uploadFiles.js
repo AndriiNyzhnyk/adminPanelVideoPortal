8809e8b4227907c4f7a42b3d553680cc914ab3b5
Andrii Nyzhnyk <andrij1510@gmail.com>
2019-11-16 21:44:28 +0200

    add component upload form (not working)

--- src/components/uploadFiles.js
+++ src/components/uploadFiles.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-const Counter = () => {
+const UploadForm = () => {
     return (
-        <div>Upload</div>
+        <form className="fileUpload" action="/upload/movie" method="post" encType="multipart/form-data">
+            <h1>Upload File Here</h1>
+            <input type="file" name="file" value=""/><br/><br/>
+            <input type="submit" />
+        </form>
     )
 };
 
-export default Counter;
+export default UploadForm;
